1082523b165d6b02e2647a83b988a733dec68b15
kentakki416 <fujiken0814@outlook.jp>
2023-07-10 08:33:29 +0900

    terraformの構築

---
+++ terraform-eks/variables.tf
@@ -0,0 +1,19 @@
+variable "repository_name" {
+  description = "eks-sample-app"
+  type = string
+}
+
+variable "github_user" {
+  description = "kentakki416"
+  type = string
+}
+
+variable "aws_account_id" {
+  description = "300126371565"
+  type = string
+}
+
+variable "my_ip_address" {
+  description = "175.177.45.186"
+  type = string
+}
